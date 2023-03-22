809e65e34e1fbef5461d2dd1bf431457733e7b63
Muhammad Duta Faturrahman <94891838+F4turrahman@users.noreply.github.com>
2023-03-22 20:29:50 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import MainScreen from './screens/MainScreen';
+import Profil from './screens/Profil';
+import Upload from './screens/Upload';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name='Profil' component={Profil} />
+        <Stack.Screen name='Upload' component={Upload} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# UTS_PAM_Tahun2023
-Nama : Muhammad Duta Faturrahman, NIM : 120140065
+# Aplikasi Biodataku
+
+Aplikasi Biodataku adalah aplikasi yang berisikan biodata dari seorang mahasiswa yang dapat diakses secara online untuk mengetahui data personal dari mahasiswa tersebut.
+
+Daftar package expo yang digunakan dalam aplikasi ini adalah
+"expo-camera": "~12.3.0",
+"expo-font": "~10.2.0",
+"expo-image-picker": "~13.3.1",
+"expo-media-library": "~14.2.0"
+
+Untuk tahap menginstall aplikasi dalam taham pengembangan adalah:
+1. Pertama user menginstall aplikasi expo di playstore/app store
+2. buka aplikasi expo tersebut lalu pilih opsi "Scan QR code'
+3. scan barcode yang ada pada file PDF.
